f6b4462e0dca736bd9ed5ea259dd76cd91a8f15c
Guilherme Massaro <guilhermecmassaro55@gmail.com>
2025-06-01 20:49:02 -0300

    new file:   Streamlit/01 Spotify.csv 	new file:   Streamlit/01_streamlit.py 	new file:   Streamlit/MaterialAula/Criando Aplicativos Web com Streamlit - Apostila Asimov Academy.pdf 	new file:   "Streamlit/MaterialAula/C\303\263digos de aula/01 Spotify.csv" 	new file:   "Streamlit/MaterialAula/C\303\263digos de aula/pages/page2.py" 	new file:   "Streamlit/MaterialAula/C\303\263digos de aula/spotify.py" 	new file:   "Streamlit/MaterialAula/Projeto Streamlit FIFA/1_\360\237\217\240_home.py" 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA17_official_data.csv 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA18_official_data.csv 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA19_official_data.csv 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA20_official_data.csv 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA21_official_data.csv 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA22_official_data.csv 	new file:   Streamlit/MaterialAula/Projeto Streamlit FIFA/datasets/CLEAN_FIFA23_official_data.csv 	new file:   "Streamlit/MaterialAula/Projeto Streamlit FIFA/pages/2_\360\237\217\203\360\237\217\274_players.py" 	new file:   "Streamlit/MaterialAula/Projeto Streamlit FIFA/pages/3_\342\232\275\357\270\217_teams.py"

---
+++ Streamlit/01_streamlit.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+
+# Page layout
+st.set_page_config(layout="wide",page_title="Spotify Streamlit App")
+
+
+
+# Dataframe
+df = pd.read_csv('01 Spotify.csv')
+df.set_index("Title", inplace=True)
+
+# Filter Artist
+artists = df["Artist"].unique()
+artist_box = st.selectbox(label = "Artista", options = artists)
+filtro = ((df["Artist"] == artist_box))
+df_filtered_by_artist = df[filtro]
+
+# Filter Album
+albums = df_filtered_by_artist["Album"].unique()
+album_box = st.selectbox(label = "Album", options = albums)
+
+
+# Gráfico de linha
+display = st.checkbox('Display chart')
+if display:
+    st.bar_chart(data = df[df["Album"] == album_box]["Stream"], horizontal=True)
+ 
