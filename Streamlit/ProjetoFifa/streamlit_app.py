bd01e1973e536e29ed24a3bb7416a030d2c2e5a3
Guilherme Massaro <guilhermecmassaro55@gmail.com>
2025-06-15 15:12:17 -0300

    modified:   Streamlit/ProjetoFifa/streamlit_app.py

--- Streamlit/ProjetoFifa/streamlit_app.py
+++ Streamlit/ProjetoFifa/streamlit_app.py
@@ -8,7 +8,8 @@ from pathlib import Path
 # Important session states
 dataset_folder = Path(__file__).parent / "datasets"
 if "fifa_data" not in st.session_state:
-    df_fifa = pd.read_csv(dataset_folder/'CLEAN_FIFA23_official_data.csv',index_col=0)
+    #df_fifa = pd.read_csv(dataset_folder/'CLEAN_FIFA23_official_data.csv',index_col=0)
+    df_fifa = pd.read_csv('datasets/CLEAN_FIFA23_official_data.csv',index_col=0)
     df_fifa = df_fifa[df_fifa["Contract Valid Until"] >= datetime.today().year]
     df_fifa = df_fifa[df_fifa["Value(£)"] > 0]
     df_fifa = df_fifa.sort_values(by="Overall",ascending=False)
@@ -19,9 +20,9 @@ if "fifa_data" not in st.session_state:
 
 # Define the pages
 pages_folder = Path(__file__).parent / "pages"
-main_page = st.Page(pages_folder/"1_🏡_Home.py")
-page_2 = st.Page(pages_folder/"2_⛹️‍♂️_Players.py")
-page_3 = st.Page(pages_folder/"3_⚽_Teams.py")
+main_page = st.Page("pages/1_🏡_Home.py")
+page_2 = st.Page("pages/2_⛹️‍♂️_Players.py")
+page_3 = st.Page("pages/3_⚽_Teams.py")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
